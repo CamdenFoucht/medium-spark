7c2cb909e5c9bdf4883385e968c143d15ccd8d80
camdenfoucht <foucht@gmail.com>
2020-09-29 13:02:24 -0700

    added favicons

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,13 +18,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Spark Medium Advertising`,
+        short_name: `Spark Medium`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#e0f3ff`,
+        theme_color: `#000`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `favicon.ico`, // This path is relative to the root of the site.
       },
     },
     // {
